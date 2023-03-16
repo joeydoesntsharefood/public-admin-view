491747b1843a6cd033258158f3aa2c4463c4a423
Mateus Sponton <mateus.smarques.silva@gmail.com>
2023-03-16 02:49:59 -0300

    sdiuasi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const withTM = require('next-transpile-modules')(['antd']);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { esmExternals: true }
 }
 
-module.exports = nextConfig
+module.exports = withTM({nextConfig})
